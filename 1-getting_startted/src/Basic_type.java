ec87a452f190b93b5cedb1e6cc8fcbf837f3fad1
Sevge <248807472@qq.com>
2020-08-10 19:24:38 +0800

    first commit

---
+++ 1-getting_startted/src/Basic_type.java
@@ -0,0 +1,23 @@
+public class Basic_type {
+    public static void main(String[] args) {
+//        八大基本数据类型
+
+//        整数
+        byte num1 = 127; //最常用
+        short num2 = 12314;
+        int num3 = 1000000000;
+        long num4 = 1000000000000L; //long类型要在数字后面加个L
+
+        // 浮点数
+        float num5 = 1.1F; //float要在数字后面加个F
+        double num6 = 3.1415926;
+
+        // 字符
+        char name = '哈';
+        // 字符串string不是关键字，是类
+//        string name2 ='hh';
+
+        boolean flag = true;
+
+    }
+}
